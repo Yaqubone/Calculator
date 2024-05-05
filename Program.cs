4dae72a4ea2eb7b77157c5673cc7638636cb0870
Yaqubone <yaqub8281@gmail.com>
2024-05-05 18:46:53 +0400

    Calculator task

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("emeliyyati sechin");
+char emeliyyat=Convert.ToChar(Console.ReadLine());
+if (emeliyyat == '+')
+{
+
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = a + b;
+    Console.WriteLine(c);
+
+}
+else if (emeliyyat == '-')
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = a - b;
+    Console.WriteLine(c);
+}
+else if (emeliyyat == '*')
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = a * b;
+    Console.WriteLine(c);
+}
+else if(emeliyyat=='/')
+{
+    int a= Convert.ToInt32(Console.ReadLine());
+    int b= Convert.ToInt32(Console.ReadLine());
+    int c = a / b;
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("emeliyyat ugursuzdur");
+}
+
